78703fa00b7211a451ec08eb3047fd65104558b0
Your Name <you@example.com>
2021-05-25 20:59:32 +0300

    add .env file

--- util/database.js
+++ util/database.js
@@ -1,13 +1,18 @@
 import mongodb from "mongodb";
 
+
 const MongoClient = mongodb.MongoClient;
 
 let _db;
 
-const client = new MongoClient("mongodb+srv://alexis:RWDHuOJXLtyHSXEt@cluster01.nk0bi.mongodb.net/Cluster01?retryWrites=true&w=majority", { useUnifiedTopology: true });
+const key_uri = process.env.CONNECTION_URI
+
+const client = new MongoClient("mongodb+srv://alexis:"+key_uri+"@cluster01.nk0bi.mongodb.net/Cluster01?retryWrites=true&w=majority", { useUnifiedTopology: true });
 
 export const connectDB = async () =>{
     try {
+
+
         await client.connect();
         console.log("Connected correctly to Cluster");
         _db = client.db();
